78985cda7ce832037157fb93572b68349ff2bb39
Nimar <l.nimar.b@gmail.com>
2013-05-10 11:11:25 +0200

    Added dynamic drawing of tick marks on the axis

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
 	};
 
 	resizeCanvas();
+
 	$(window).resize(function() {
 		resizeCanvas();
 		graph.plotIt();
@@ -37,11 +38,16 @@ $(document).ready(function() {
 		this.maxX = cfg.maxX;
 		this.maxY = cfg.maxY;
 
+		this.tickRate = (this.maxX + this.maxY) / 20;
+		this.tickSize = 6;
+		this.tickFont = '8pt Verdana';
+
 		this.ctx = this.can.getContext('2d');
+		//seperation redundant because always square?
 		this.rangeX = this.maxX - this.minX;
 		this.rangeY = this.maxY - this.minY;
-		this.unitX = this.can.width / this.rangeX;
-		this.unitY = this.can.height / this.rangeY;
+		this.pUnitX = this.can.width / this.rangeX;
+		this.pUnitY = this.can.height / this.rangeY;
 		this.centerY = Math.round(Math.abs(this.minY / this.rangeY) * this.can.height);
 		this.centerX = Math.round(Math.abs(this.minX / this.rangeX) * this.can.width);
 		this.iter = (this.maxX - this.minX) / 1000;
@@ -62,6 +68,10 @@ $(document).ready(function() {
 	Plot.prototype.setUp = function() {
 		this.centerY = Math.round(Math.abs(this.minY / this.rangeY) * this.can.height);
 		this.centerX = Math.round(Math.abs(this.minX / this.rangeX) * this.can.width);
+		this.rangeX = this.maxX - this.minX;
+		this.rangeY = this.maxY - this.minY;
+		this.pUnitX = this.can.width / this.rangeX;
+		this.pUnitY = this.can.height / this.rangeY;
 		this.scaleX = this.can.width / this.rangeX;
 		this.scaleY = this.can.height / this.rangeY;
 		this.axisLW = 1;
@@ -78,16 +88,67 @@ $(document).ready(function() {
 			case "x":
 				ctx.moveTo(0, this.centerY);
 				ctx.lineTo(this.can.width, this.centerY);
+				ctx.lineWidth = this.axisLW;
+				ctx.stroke();
+				this.drawTickMarks(axis);
 				break;
 			case "y":
 				ctx.moveTo(this.centerX, 0);
 				ctx.lineTo(this.centerX, this.can.height);
+				ctx.lineWidth = this.axisLW;
+				ctx.stroke();
+				this.drawTickMarks(axis);
 				break;
 			default:
 				break;
 		}
-		ctx.lineWidth = this.axisLW;
-		ctx.stroke();
+
+		ctx.restore();
+	};
+
+	Plot.prototype.drawTickMarks = function(axis) {
+		var ctx = this.ctx;
+		ctx.save();
+		ctx.beginPath();
+		var incr, pos, unit;
+		switch(axis) {
+			case "x":
+				incr = this.tickRate * this.pUnitX;
+				pos = 0 + incr;
+				unit = this.minX + this.tickRate;
+				while(pos < this.can.width) {
+					ctx.moveTo(pos, this.centerY - this.tickSize);
+					ctx.lineTo(pos, this.centerY + this.tickSize);
+					ctx.stroke();
+					ctx.fillText(unit, pos, this.centerY + this.tickSize + 3);
+					if(unit + this.tickRate >= 0 && unit < 0) {
+						pos = this.centerX ;
+						unit += this.tickRate;
+					}
+					unit = Math.round(unit + this.tickRate);
+					pos = Math.round(pos + incr);
+				}
+				break;
+			case "y":
+				incr = this.tickRate * this.pUnitY;
+				pos = this.can.height - incr;
+				unit = this.minY + this.tickRate;
+				while(pos > 0) {
+					ctx.moveTo(this.centerX - this.tickSize, pos);
+					ctx.lineTo(this.centerX + this.tickSize, pos);
+					ctx.stroke();
+					ctx.fillText(unit, this.centerX + this.tickSize + 3, pos);
+					if(unit + this.tickRate >= 0 && unit < 0) {
+						pos = this.centerY;
+						unit += this.tickRate;
+					}
+					unit = Math.round(unit + this.tickRate);
+					pos = Math.round(pos - incr);
+				}
+				break;
+			default:
+				break
+		}
 		ctx.restore();
 	};
 
